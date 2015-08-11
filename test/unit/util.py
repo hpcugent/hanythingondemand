209a3299f2ea686b99310578320762c398990902
Ewan Higgs <ewan.higgs@ugent.be>
2015-08-11 23:52:30 +0200

    Add two new test files!

---
+++ test/unit/util.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# #
+# Copyright 2009-2015 Ghent University
+#
+# This file is part of hanythingondemand
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/hanythingondemand
+#
+# hanythingondemand is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# hanythingondemand is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with hanythingondemand. If not, see <http://www.gnu.org/licenses/>.
+# #
+"""
+Utilities for testing.
+
+@author: Ewan Higgs (Universiteit Gent)
+"""
+
+import sys
+from cStringIO import StringIO
+from contextlib import contextmanager
+
+@contextmanager
+def capture(command, *args, **kwargs):
+    """Capture stdout in a context manager"""
+    out, sys.stdout = sys.stdout, StringIO()
+    command(*args, **kwargs)
+    sys.stdout.seek(0)
+    yield sys.stdout.read()
+    sys.stdout = out
+
+
