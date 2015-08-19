5ebbfdbc4dad63ffe2c02ecc09ba600c34127105
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-08-19 11:18:42 +0200

    bump versions for IPython, Spark, Hadoop

--- etc/hod/IPython-notebook-3.2.1/start-notebook.sh
+++ etc/hod/IPython-notebook-3.2.1/start-notebook.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-module load IPython/3.0.0-intel-2015a-Python-2.7.9
-module load Python/2.7.9-intel-2015a
-module load matplotlib/1.4.3-intel-2015a-Python-2.7.9
-module load Spark/1.3.0
+module load IPython/3.2.1-intel-2015a-Python-2.7.10
+module load matplotlib/1.4.3-intel-2015a-Python-2.7.10
+module load Spark/1.4.1
 
 ipython profile create nbserver
 
@@ -25,7 +24,7 @@ EOF
 # Add the PySpark classes to the Python path (taken from pyspark script):
 export SPARK_HOME=$EBROOTSPARK
 export PYTHONPATH="$SPARK_HOME/python/:$PYTHONPATH"
-export PYTHONPATH="$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$PYTHONPATH"
+export PYTHONPATH="$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$PYTHONPATH"  # FIXME?
 
 export PYTHONPATH=$PYTHONPATH:$EBROOTPYTHON/lib/python2.7/site-packages/ 
 
