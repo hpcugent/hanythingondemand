1ce0551d10aea201d338b5fab01c522ec795fdfe
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-10-20 21:32:12 +0200

    fix remarks

--- setup.py
+++ setup.py
@@ -73,10 +73,13 @@ PACKAGE = {
         'Programming Language :: Python :: 2'
     ],
     'install_requires': [
-# Pin vsc-base to 2.2.2 since it's the last non broken tag.
-        'vsc-base >= 2.2.2',
+        # stick with vsc-base 2.2.2 for now;
+        # last tagged release before split between vsc-base and vsc-install causing problems
+        # note: vsc-base version is also pinned in setup.py!
+        'vsc-base == 2.2.2',
         'setuptools',
-# Pin mpi4py to 1.3.1 since it's the last of the 1.x series and we don't want to pick up 2.0.0
+        # pin mpi4py to 1.3.1 since it's the last of the 1.x series and we don't want to pick up 2.0.0
+        # note: mpi4py version is also pinned in setup.py!
         'mpi4py == 1.3.1',
         'pbs-python',
         'netifaces',
